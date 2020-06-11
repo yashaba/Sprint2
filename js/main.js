8d32ae3f0e3bbb5dc2a5a874dc9f51bfdbdefc4d
yashaba <yashabazovner@gmail.com>
2020-06-11 11:29:15 +0300

    third commit

--- js/main.js
+++ js/main.js
@@ -49,16 +49,24 @@ function drawText(x, y) {
     let size = document.querySelector('#size').value
     gLines[0].txt = document.querySelector('.input1').value
     gLines[1].txt = document.querySelector('.input2').value
-    let text2 = document.querySelector('.input2').value
     gCtx.lineWidth = '3';
     gCtx.strokeStyle = 'black';
     gCtx.fillStyle = 'white';
-    gCtx.font = `${size}px IMPACT`;
     gCtx.textAlign = 'center';
+
+
+    gCtx.font = `${gLines[0].size}px IMPACT`;
     gCtx.fillText(gLines[0].txt, gLines[0].x, gLines[0].y);
     gCtx.strokeText(gLines[0].txt, gLines[0].x, gLines[0].y);
+
+    gLines[0].width = gCtx.measureText(gLines[0].txt).width;
+
+
+    gCtx.font = `${gLines[1].size}px IMPACT`;
     gCtx.fillText(gLines[1].txt, gLines[1].x, gLines[1].y);
     gCtx.strokeText(gLines[1].txt, gLines[1].x, gLines[1].y);
+
+    gLines[1].width = gCtx.measureText(gLines[1].txt).width;
 
 
 }
@@ -66,15 +74,28 @@ function drawText(x, y) {
 
 function setSize() {
     document.querySelector('#size').value = document.querySelector('#range').value
+
+    gLines.forEach(line => line.size = document.querySelector('#size').value)
     drawText()
+    if (gSelectedLine) {
+        //debugger
+        gSelectedLine.size = document.querySelector('#size').value
+        drawRect(gSelectedLine)
+    }
+
+
 }
 
 function onMoveUp() {
-
+    gSelectedLine.y -= 10
+    drawText()
+    drawRect(gSelectedLine)
 }
 
 function onMoveDown() {
-
+    gSelectedLine.y += 10
+    drawText()
+    drawRect(gSelectedLine)
 }
 
 
@@ -83,35 +104,39 @@ var gLines = [{
         size: 55,
         x: 250,
         y: 50,
-        color: 'white'
+        color: 'white',
+        width: ''
     },
     {
         txt: '',
         size: 55,
         x: 250,
         y: 450,
-        color: 'white'
+        color: 'white',
+        width: ''
     }
 ]
 
-
+var gSelectedLine
 
 function canvasClicked(ev) {
-    // TODO: find out if clicked inside of star chart
     const { offsetX: x, offsetY: y } = ev;
 
     var clickedLine = gLines.find((line, idx) => {
         //debugger
-        if (x >= line.x - line.size && x <= line.size + line.x) return line
+        if (x >= line.x - line.width / 2 && x < line.x + line.width / 2 && y > line.y - line.size + 11 && y < line.y) return line
     });
-    console.log(clickedLine);
-    console.log({ offsetX: x, offsetY: y });
+    gSelectedLine = clickedLine
+    if (gSelectedLine) drawRect(gSelectedLine)
+}
 
-
-    // TODO: open the modal on the clicked coordinates if found a click on a star,
-    // close the modal otherwise
-
-    // const { clientX, clientY } = ev;
-    // if (clickedStar) openModal(clickedStar, clientX, clientY);
-    // else closeModal();
+function drawRect(line) {
+    //debugger
+    gCtx.beginPath();
+    gCtx.rect(line.x - line.width / 2 - 5, line.y - line.size, line.width + 10, line.size + 10);
+    // gCtx.rect(x-75, y-75, 150, 150);
+    gCtx.strokeStyle = 'black';
+    gCtx.stroke();
+    gCtx.fillStyle = 'orange';
+    //gCtx.fillRect(x, y, 150 / 2, 150 / 2);
 }
